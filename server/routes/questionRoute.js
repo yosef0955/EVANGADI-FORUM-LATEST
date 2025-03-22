65657b82c4c68d8a8f9fc220601d4f0d76a1f792
Gedefaw-T <gedefawtigabum@gmail.com>
2025-03-23 01:41:01 +0300

    Implemented API Endpoint: Get Single Question

--- server/routes/questionRoute.js
+++ server/routes/questionRoute.js
@@ -4,7 +4,9 @@ const authHeader = require("../MiddleWare/authMiddleware");
 const {
   allQuestions,
   postQuestion,
+  singleQuestion
 } = require("../controller/questionController");
 router.get("/all-questions", authHeader, allQuestions);
 router.post("/post-question", authHeader, postQuestion);
+router.get("/:question_id", authHeader, singleQuestion);
 module.exports = router;
