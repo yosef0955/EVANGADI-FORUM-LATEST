ab9dab339f89b7f2cd7ec1d47efdc6afded250ab
Mohammed <mamegit2020@gmail.com>
2025-03-23 18:19:16 +0300

    Implemented API Endpoint: Get Answers for a Question

--- server/routes/answerRoute.js
+++ server/routes/answerRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const authHeader = require("../MiddleWare/authMiddleware");
-const { postAnswer } = require("../controller/answerController");
+const { postAnswer, getAnswer } = require("../controller/answerController");
 
 router.post("/post-answers/:question_id", authHeader, postAnswer);
+router.get("/:question_id", authHeader, getAnswer);
 module.exports = router;
